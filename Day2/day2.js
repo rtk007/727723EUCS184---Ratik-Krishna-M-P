d1964bc499e1db9ce5b29d53ed4d3f9ab461019a
rtk007 <138106778+rtk007@users.noreply.github.com>
2024-07-31 16:22:52 +0530

    Add files via upload

---
+++ Day2/day2.js
@@ -0,0 +1,11 @@
+
+
+function Day2()
+{
+    return(
+        <div className="App" style={{border: '10px solid black',backgroundColor:"cyan",height:"300px",}}>
+            <h1 style={{color:"green"}}>Let we see the output of JAVASCRIPT</h1>
+            <p style={{color:"darkblue"}}>lorem epsum dolit van der dussen</p>
+        </div>
+    )
+}export default Day2;
