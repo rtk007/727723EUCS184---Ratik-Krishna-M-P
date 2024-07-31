9e43ceed1a495ded7223f9c15a1632b7cf1f246d
rtk007 <138106778+rtk007@users.noreply.github.com>
2024-07-31 15:59:55 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,15 @@
+function classs()
+{
+    alert("Message from javascript Alert!")
+    console.log("Message from javascript Alert!")
+}
+
+function Day1()
+{
+    return(
+        <div className="App">
+            <h1>Let we see the output of JAVASCRIPT</h1>
+                <button onClick={classs}>JAVASCRIPT</button>
+        </div>
+    )
+}export default Day1;
